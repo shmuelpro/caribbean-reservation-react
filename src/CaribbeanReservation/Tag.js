43dcdad8fba08044f3a3a628214bfafb75109fb3
shmuel PC <shmuelpro@gmail.com>
2019-10-16 16:58:22 +0300

    added id to on mouse over tag

--- src/CaribbeanReservation/Tag.js
+++ src/CaribbeanReservation/Tag.js
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from 'react';
 
 export default function Tag(props) {
 
-    return (<div onClick={props.onClick} onMouseOver={props.onMouseOver} style={props.style} className={props.className} >
+    return (<div onClick={props.onClick} onMouseOver={()=>{props.onMouseOver(props.id)}} style={props.style} className={props.className} >
         {
             [...Array(props.length)].map((x, i) => {
                 return <div key={i} style={{ width: props.dimension, height: props.dimension, pointerEvents: "none" }}></div>
